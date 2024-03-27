6b0f49fc1b042d28c75e1c8ecb02230ca7aa639c
BlankDev117 <mmmasent@gmail.com>
2024-03-27 02:26:59 -0400

    Added mock output project (OSK-7) (#8)
    * Added mock output project (OSK-7)
    
    Motivation
    ----
    There is a need to allow unit tests access to a mock output factory since the current ones are internal and provide extra valdiation that may be unnecessary to unit tests
    
    Modifications
    ----
    * Added mock project with limited validation for unit test consumption
    
    Result
    ----
    Unit tests may access a MockOutputFactory
    
    * That solution file..

---
+++ src/OSK.Functions.Outputs.Mocks/MockOutput`1.cs
@@ -0,0 +1,12 @@
+﻿using OSK.Functions.Outputs.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OSK.Functions.Outputs.Mocks
+{
+    public class MockOutput<T> : MockOutput, IOutput<T>
+    {
+        public T Value { get; set; }
+    }
+}
