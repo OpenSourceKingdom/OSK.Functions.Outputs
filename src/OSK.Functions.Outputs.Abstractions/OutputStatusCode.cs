dd90cdcb44e5e9c5271de3ae3d54cfde563fafd4
BlankDev117 <mmmasent@gmail.com>
2024-04-10 16:21:29 -0400

    Update OutputStatusCode toString (OSK-outputs-13) (#14)
    Motivation
    ----
    Unless specifically requested, the output for the OutputStatusCode should not include the origination source and it should be the numeric committed
    
    Modifications
    ----
    * Added static helper for parsing OutputStatusCode's from strings
    * Added toString override to determine if origination source should be included
    * Added UnitTests
    
    Result
    ----
    OutputStatusCode toString will only return the origination source if requested

--- src/OSK.Functions.Outputs.Abstractions/OutputStatusCode.cs
+++ src/OSK.Functions.Outputs.Abstractions/OutputStatusCode.cs
@@ -1,4 +1,5 @@
-﻿using System.Net;
+﻿using System;
+using System.Net;
 
 namespace OSK.Functions.Outputs.Abstractions
 {
@@ -20,15 +21,53 @@ namespace OSK.Functions.Outputs.Abstractions
 
         public string OriginationSource { get; }
 
+        private readonly string _codeSuffix; 
+
         #endregion
 
         #region Constructors
 
-        public OutputStatusCode(HttpStatusCode statusCode, DetailCode detailCode, string originationSource = DefaultSource)
+        public OutputStatusCode(HttpStatusCode statusCode, DetailCode detailCode, 
+            string originationSource = DefaultSource)
         {
             StatusCode = statusCode;
             DetailCode = detailCode;
             OriginationSource = originationSource;
+            _codeSuffix = string.IsNullOrWhiteSpace(originationSource) || originationSource == DefaultSource
+                ? string.Empty
+                : $",{originationSource}";
+        }
+
+        public static OutputStatusCode Parse(string statusCode)
+        {
+            var statusCodeParts = statusCode.Split(',');
+            if (statusCodeParts.Length < 1 || statusCodeParts.Length > 2)
+            {
+                throw new InvalidOperationException("The expected status code parts were not found.");
+            }
+
+            var statusParts = statusCodeParts[0].Split(".");
+            if (statusParts.Length != 2)
+            {
+                throw new InvalidOperationException("The expected status parts were not found.");
+            }
+
+            if (!int.TryParse(statusParts[0], out var httpStatusCode) 
+                && !Enum.IsDefined(typeof(HttpStatusCode), httpStatusCode))
+            {
+                throw new InvalidOperationException("The expected http status code was not valid.");
+            }
+            if (!int.TryParse(statusParts[1], out var detailCode) 
+                && !Enum.IsDefined(typeof(DetailCode), detailCode))
+            {
+                throw new InvalidOperationException("The expected detail code was not valid.");
+            }
+
+            var originationSource = statusCodeParts.Length == 1
+                ? DefaultSource
+                : statusCodeParts[1];
+
+            return new OutputStatusCode((HttpStatusCode)httpStatusCode, (DetailCode)detailCode, originationSource);
         }
 
         #endregion
@@ -37,7 +76,11 @@ namespace OSK.Functions.Outputs.Abstractions
 
         public bool IsSuccessCode => StatusCode >= HttpStatusCode.OK && StatusCode < HttpStatusCode.MultipleChoices;
 
-        public override string ToString() => $"{(int)StatusCode}.{DetailCode}";
+        public override string ToString() => $"{(int)StatusCode}.{(int)DetailCode}";
+
+        public string ToString(bool includeOrigination) => includeOrigination
+            ? $"{(int)StatusCode}.{(int)DetailCode},{OriginationSource}"
+            : ToString();
 
         #endregion
     }
