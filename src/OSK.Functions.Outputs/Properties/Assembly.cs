adac418dc965d93136e7ecf247d0aea1da4b3025
BlankDev117 <mmmasent@gmail.com>
2025-01-07 17:24:11 -0500

    Fix Mocks (OSK-23) (#24)
    Motivation
    ----
    Mocks are currently borked due to some missing implementation
    
    Modifications
    ----
    * Added missing implementation
    
    Result
    ----
    Mocks work as intended
    
    Fixes #23

--- src/OSK.Functions.Outputs/Properties/Assembly.cs
+++ src/OSK.Functions.Outputs/Properties/Assembly.cs
@@ -1,5 +1,7 @@
 ﻿using System.Runtime.CompilerServices;
 
+[assembly: InternalsVisibleTo("OSK.Functions.Outputs.Mocks")]
 [assembly: InternalsVisibleTo("OSK.Functions.Outputs.Logging")]
 [assembly: InternalsVisibleTo("OSK.Functions.Outputs.Logging.UnitTests")]
+[assembly: InternalsVisibleTo("OSK.Functions.Outputs.Mocks")]
 [assembly: InternalsVisibleTo("OSK.Functions.Outputs.UnitTests")]
